896eb7310513eb4abe2ef6813021b1e0fcc29a50
ARAVINTH <aravinths911@gmail.com>
2024-05-16 22:57:08 +0530

    root file changed for backend

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,13 @@ app.use('/api/user', userRoutes)
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+    console.log("Database connected");
+    
   })
   .catch((error) => {
     console.log(error)
+  })
+
+  app.listen(process.env.PORT, () => {
+    console.log('connected to db & listening on port', process.env.PORT)
   })
